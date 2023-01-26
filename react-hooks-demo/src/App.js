01f94aaa24006cd540d87e173e5062a9f0f44c08
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2023-01-26 11:21:27 +0100

    Add simpple and complex reducer

--- react-hooks-demo/src/App.js
+++ react-hooks-demo/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import CounterTwo from './components/CounterTwo';
+// import CounterOne from './components/CounterOne';
 // import ClassCounter from './components/ClassCounter';
-import DataFetching from './components/DataFetching';
+// import DataFetching from './components/DataFetching';
 // import HookCounterFour from './components/HookCounterFour';
 // import HookCounterThree from './components/HookCounterThree';
 // import HookCounterTwo from './components/HookCounterTwo';
@@ -13,7 +15,7 @@ import DataFetching from './components/DataFetching';
 function App() {
   return (
     <div className="App">
-      <DataFetching />
+      <CounterTwo />
     </div>
   );
 }
