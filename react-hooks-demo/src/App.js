c2058001ab463db72e41c1b58c458c1f9b43f449
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2023-01-29 14:23:14 +0100

    Add useRef hook

--- react-hooks-demo/src/App.js
+++ react-hooks-demo/src/App.js
@@ -1,11 +1,11 @@
-import React  from 'react'
+import React, { useEffect }  from 'react'
 import './App.css';
 import FocusInput from './components/FocusInput';
 
 
 
 const  App= ()=>  {
-
+ 
   return (
     <div className='App'>
       <FocusInput/>
--- react-hooks-demo/src/components/FocusInput.js
+++ react-hooks-demo/src/components/FocusInput.js
@@ -3,14 +3,22 @@ import React, { useEffect, useRef } from 'react'
 const FocusInput = () => {
 
   const inputRef = useRef(null)
+  const divRef = useRef(null)
+  console.log(inputRef);
   useEffect(()=>{
     inputRef.current.focus()
   }, [])
+
+  const handleClick = ()=>{
+    console.log('Hello');
+    inputRef.current.focus()
+  }
 
   return (
     <div>
       <input type="text" value="test"/>
       <input type="text" ref={inputRef}/>
+      <div onClick={handleClick} ref={divRef}>Alert pop up</div>
     </div>
   )
 }
