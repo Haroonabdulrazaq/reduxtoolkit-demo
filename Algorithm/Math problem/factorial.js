a7f3a3c3b2665bb86dc0b44564c0e5d0cf294bb4
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2023-02-13 17:35:22 +0100

    Add recursion to fibonnaci seuesnce

--- Algorithm/Math problem/factorial.js
+++ Algorithm/Math problem/factorial.js
@@ -1,26 +1,33 @@
-const factorial = (n)=>{
-  let product = 1
-  for(let i=1; i <= n; i++){
-    product *=i
-    console.log('R', product);
-  }
-  // console.log(product);
-}
+
+
+
+
+
+// Solutions Using Loop
+
+// const factorial = (n)=>{
+//   let product = 1
+//   for(let i=1; i <= n; i++){
+//     product *=i
+//     console.log('R', product);
+//   }
+//   console.log(product);
+// }
 // factorial(0)
 // factorial(1)
-factorial(5)
-factorial(10)
+// factorial(5)
+// factorial(10)
 
-const factorialD = (n)=>{
-  let product = 1
-  for(let i=2; i <= n; i++){
-    product *=i
-    console.log('D', product);
-  }
-  // console.log(product);
-}
+// const factorialD = (n)=>{
+//   let product = 1
+//   for(let i=2; i <= n; i++){
+//     product *=i
+//     console.log('D', product);
+//   }
+//   console.log(product);
+// }
 
 // factorialD(0)
 // factorialD(1)
-factorialD(5)
-factorialD(10)
+// factorialD(5)
+// factorialD(10)
--- Algorithm/Math problem/fibonacci.js
+++ Algorithm/Math problem/fibonacci.js
@@ -1,3 +1,21 @@
+// Solution Using Recursion
+const fib = (n)=>{
+  if(n<2){
+    return 1
+  }
+  const fibAnswer = fib(n-1) + fib(n-2)
+  return fibAnswer
+}
+
+console.log(fib(0))
+console.log(fib(1))
+console.log(fib(2))
+console.log(fib(3))
+console.log(fib(4))
+console.log(fib(5))
+console.log(fib(6))
+
+// Solution Using Lopp or Iteration
 const fibonacci = (num) =>{
   let fib = [0, 1]
   for(let i=2; i < num; i++){
@@ -8,4 +26,5 @@ const fibonacci = (num) =>{
 }
 fibonacci(5)
 
-// The Big-O is Linear -- it has a single for loop
+// // The Big-O is Linear O(n) -- it has a single for loop
+// // The Big-O is Exponential O(2^n) -- it has terrible perfomance as the input grows
