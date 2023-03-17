cf78bbfa588595159ac515adbe7d116c9733f757
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2023-03-17 12:36:30 +0100

    Add matcher getByLabelText

--- react-testing/src/components/application/Application.test.tsx
+++ react-testing/src/components/application/Application.test.tsx
@@ -5,17 +5,39 @@ import Application from "./Application";
 describe('Application Test', ()=>{
   test('Renders correctly',()=>{
     render(<Application/>);
-    let textBoxElement =  screen.getByRole("namebox")
-    expect(textBoxElement).toBeInTheDocument()
+
+    const pageHeading = screen.getByRole('heading', {
+      level: 1 
+    })
+    expect(pageHeading).toBeInTheDocument()
+    const sectionHeading = screen.getByRole('heading', { 
+      level: 2
+    })
+    expect(sectionHeading).toBeInTheDocument()
+
+    let nameElement =  screen.getByRole("textbox", {
+      name: 'Name'
+    })
+    expect(nameElement).toBeInTheDocument()
+
+    const nameElement2 = screen.getByLabelText('Name', {
+      selector: 'input'
+    })
+    expect(nameElement2).toBeInTheDocument()
+
+    let bioElement = screen.getByRole("textbox", {
+      name: 'Bio'
+    })
+    expect(bioElement).toBeInTheDocument()
 
     let jobLocationElement = screen.getByRole("combobox")
     expect(jobLocationElement).toBeInTheDocument()
-
-    let textAreaElement = screen.getByRole("textbox")
-    expect(textAreaElement).toBeInTheDocument()
     
     let checkBoxElement = screen.getByRole("checkbox")
     expect(checkBoxElement).toBeInTheDocument()
+
+    const checkBoxElement2 = screen.getByLabelText("I agree to the terms and conditions")
+    expect(checkBoxElement2).toBeInTheDocument()
 
     let buttonElement = screen.getByRole("button")
     expect(buttonElement).toBeInTheDocument()
--- react-testing/src/components/application/Application.tsx
+++ react-testing/src/components/application/Application.tsx
@@ -15,7 +15,7 @@ const Application = () => {
             id="name"
             placeholder="Fullname"
             value="Vishwas"
-            role="namebox"
+            // role="namebox"
             onChange={() => {}}
           />
         </div>
@@ -24,7 +24,7 @@ const Application = () => {
           <textarea id="bio" />
         </div>
         <div>
-          <label htmlFor="job-location">Job location</label>
+          <label htmlFor="job-location">Name</label>
           <select id="job-location">
             <option value="">Select a country</option>
             <option value="US">United States</option>
