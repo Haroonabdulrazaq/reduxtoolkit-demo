91e2e8d9c07c251a79a2b52b08e2d197e4a593d6
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2023-03-25 22:19:05 +0100

    Add all controllers to project

--- workout-app/backend/controllers/workoutcontroller.js
+++ workout-app/backend/controllers/workoutcontroller.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
 import Workout from '../models/workoutsmodel.js'
 
+
+//GET  all Workouts
 export const getAllWorkouts = async(req, res)=>{
   try {
     const workouts = await Workout.find().sort({createdAt: -1})
@@ -10,6 +12,7 @@ export const getAllWorkouts = async(req, res)=>{
   }
 }
 
+//CREATE a Workout 
 export const createWorkout = async(req, res)=>{
   const {title, reps, load } = req.body
   try{
@@ -20,18 +23,63 @@ export const createWorkout = async(req, res)=>{
   }
 }
 
+//GET  a Single Workout 
 export const getWorkout = async(req, res) =>{
   const {id} = req.params
   if(!mongoose.Types.ObjectId.isValid(id)){
    return res.status(404).json({error: 'No such workout'})
   }
   try {
-    const workout = await Workout.findById(req.params.id)
+    const workout = await Workout.findById({_id: req.params.id})
     if(!workout) {
       return res.status(404).json({message: 'Workout does not exist'})
     }
     res.status(200).json({message: 'Successfull', data: workout})
   }catch(error) {
-    return res.status(400).json({message: 'Oops, an error occured while fetching workout', error: error.message})
+    return res.status(400).json({message: 'Oops! an error occured while fetching workout', error: error.message})
+  }
+}
+
+// DELETE a Workout
+export const deleteWorkout = async(req, res)=>{
+  const {id} = req.params
+  if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(404).json({error: 'No such workout'})
+  }
+  try{
+    const workout = await Workout.findOneAndDelete({_id: id})
+    if(!workout) {
+      return res.status(404).json({message: 'Workout does not exist'})
+    }
+    res.status(200).json({message: 'Deleted successfully', workout})
+  }catch(error) {
+    res.status(400).json({
+      message: 'Oops!, an error occured while deleting workout',
+      error: error.message
+    })
+  }
+}
+
+// UPDATE a Workout
+
+export const updateWorkout = async(req, res) =>{
+  const {title, reps, load } = req.body
+  const {id}  = req.params
+  if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(404).json({message: 'No such workout'})
+  }
+
+  try{
+    const workout = await Workout.findOneAndUpdate(
+      {_id: id},
+      {...req.body},
+      {new: true }  // Same as {returnOriginal: false}
+    )
+    if(!workout) {
+      return res.status(404).json({message: 'Workout does not exist'})
+    }
+    res.status(201).json({message: 'Updated Successfully', workout})
+  }catch(error) {
+    res.status(400).json({message: 'Oops!, an error occured, while updating workout'})
   }
 }
--- workout-app/backend/routes/workout.js
+++ workout-app/backend/routes/workout.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createWorkout, getAllWorkouts, getWorkout } from '../controllers/workoutcontroller.js';
+import { createWorkout, deleteWorkout, getAllWorkouts, getWorkout, updateWorkout } from '../controllers/workoutcontroller.js';
 
 
 const workoutRouter = express.Router();
@@ -16,15 +16,9 @@ workoutRouter.post('/', createWorkout)
 
 
 // DELETE a workout 
-workoutRouter.delete('/:id', (req, res)=>{
-  console.log('Delete a particular workout with id');
-  res.json({status: 201, message: 'Delete a workout with id'})
-})
+workoutRouter.delete('/:id', deleteWorkout)
 
 // UPDATE  a workout 
-workoutRouter.patch('/:id', (req, res)=>{
-  console.log('Post to workouts');
-  res.json({status: 201, message: 'Edit a workout with id'})
-})
+workoutRouter.patch('/:id', updateWorkout)
 
 export default workoutRouter;
