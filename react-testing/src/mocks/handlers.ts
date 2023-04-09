47800c288446aac774aef24d4cfd8becd2e69a71
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2023-04-09 15:40:26 +0100

    Add error handling for HTTP test

--- react-testing/src/mocks/handlers.ts
+++ react-testing/src/mocks/handlers.ts
@@ -2,7 +2,8 @@ import {rest} from 'msw'
 
 export const handlers = [
   rest.get('https://jsonplaceholder.typicode.com/users', (req, res, ctx)=>{
-  return res(ctx.status(200),
+  return res(
+    ctx.status(200),
     ctx.json([
       {name: 'Bruce wayne'},
       {name: 'Clerk Kent'},
