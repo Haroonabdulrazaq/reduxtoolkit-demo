8956f3ac5817831c59d842c617c0b0abdbef051e
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2024-01-24 18:23:44 +0100

    Add dynamoc routes

---
+++ next-14-demo/routing-demo/src/app/products/pages.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const pages = () => {
+  return (
+    <>
+      <h1>Product List</h1>
+      <h2>Product List 1</h2>
+      <h2>Product List 2</h2>
+      <h2>Product List 3</h2>
+    </>
+  );
+};
+
+export default pages;
