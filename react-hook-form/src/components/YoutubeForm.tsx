1712660e6fa715fd55bf3dc9378983acfaa71f93
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2024-05-28 11:11:41 +0100

    Add defaultValue to form

--- react-hook-form/src/components/YoutubeForm.tsx
+++ react-hook-form/src/components/YoutubeForm.tsx
@@ -9,11 +9,18 @@ type FormValues = {
 
 export const YouTubeForm = () => {
 
-  const form = useForm<FormValues>();
-  const { register, control, handleSubmit } = form;
-
+  const form = useForm<FormValues>({
+    defaultValues: {
+        username: 'Vishwas',
+        email: '',
+        channel: ''
+    }
+  });
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
   const onSubmit = (data: FormValues) => {
     console.log(data);
+    console.log(formState);
   }
 
   return (
@@ -21,17 +28,37 @@ export const YouTubeForm = () => {
       <h1>YouTube Form</h1>
 
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" {...register("username", {
-          required: "Username is required",
-        })} />
-
-        <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" {...register("email")} />
-
-        <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" {...register("channel")} />
-
+        <div className='form-control'>    
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" {...register("username", {
+            required: "Username is required",
+          })} />
+          <p className='error'>{errors.username?.message}</p>
+        </div>
+        <div className='form-control'>         
+          <label htmlFor="email">E-mail</label>
+          <input type="email" id="email" {...register("email", {
+            pattern: {
+            value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              message: "Invalid email format"
+            }, validate: {
+              notAdmin: (fieldValue) => {
+                return ( fieldValue !== 'admin@example.com' || 'Enter a different email address')
+              },
+              notBlacklisted: (fieldValue) => {
+                return ( !fieldValue.endsWith('@baddomain.com') || 'This domain is not supported!')
+              }
+            }
+          })} />
+          <p className='error'>{errors.email?.message}</p>
+        </div> 
+        <div className='form-control'>
+          <label htmlFor="channel">Channel</label>
+          <input type="text" id="channel" {...register("channel", {
+            required: "Channel is required",
+          })} />
+          <p className='error'>{errors.channel?.message}</p>
+        </div>  
         <button>Submit</button>
       </form>
       <DevTool control={control}/>
