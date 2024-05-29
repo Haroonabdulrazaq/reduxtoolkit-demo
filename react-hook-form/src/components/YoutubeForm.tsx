5fd7014d4f9fbe328867bd6de251adf51e102ef7
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2024-05-29 22:01:35 +0100

    Add getValues from form

--- react-hook-form/src/components/YoutubeForm.tsx
+++ react-hook-form/src/components/YoutubeForm.tsx
@@ -1,4 +1,5 @@
-import { useForm } from 'react-hook-form';
+import { useEffect } from 'react';
+import { useForm, useFieldArray } from 'react-hook-form';
 import { DevTool } from '@hookform/devtools';
 
 type FormValues = {
@@ -8,32 +9,61 @@ type FormValues = {
   social: {
     twitter: string,
     facebook: string
-  }
+  },
+  phoneNumbers: string[],
+  phNumbers: {
+    number: string
+  }[],
+  age: number,
+  dob: Date
 };
 
 export const YouTubeForm = () => {
 
   const form = useForm<FormValues>({
     defaultValues: {
-        username: 'Vishwas',
-        email: '',
-        channel: '',
-        social: {
-          twitter: '',
-          facebook: ''
-        }
+      username: 'Vishwas',
+      email: 'Vishwas@mail.com',
+      channel: 'Codevolution',
+      social: {
+        twitter: '@Vishwas',
+        facebook: 'Vishwasprofile'
+      },
+      phoneNumbers: ["1234345542", "679434345542"],
+      phNumbers: [{number: ''}],
+      age: 0,
+      dob: new Date(), // Date Tue May 28 2024 15:42:20 GMT+0100
     }
   });
-  const { register, control, handleSubmit, formState } = form;
+
+  const { register, control, handleSubmit, formState, watch, getValues } = form;
   const { errors } = formState;
+
+  // const watchUsername = watch();
+
+ const { fields, append, remove } = useFieldArray({
+    name: 'phNumbers',
+    control 
+  })
+
+  const handleGetValues = () =>{
+    console.log('GetValues', getValues("social.facebook"));
+  }
   const onSubmit = (data: FormValues) => {
     console.log(data);
-    console.log(formState);
   }
+
+  useEffect(()=>{
+    const subscribe = watch((value)=> {
+      console.log(value);
+    });
+    return ()=> subscribe.unsubscribe();
+  }, [watch])
 
   return (
     <div>
-      <h1>YouTube Form</h1>
+      <h1>YouTube Form </h1>
+      {/* <h3>Watched Value {JSON.stringify(watchUsername)}</h3> */}
 
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <div className='form-control'>    
@@ -66,16 +96,71 @@ export const YouTubeForm = () => {
             required: "Channel is required",
           })} />
           <p className='error'>{errors.channel?.message}</p>
-        </div>  
+        </div>
         <div className='form-control'>
           <label htmlFor="twitter">Twitter</label>
-          <input type="text" id="twitter" {...register("social.twitter")} />
+          <input type="text" id="twitter" {...register("social.twitter", {
+            required: {
+             value: true,
+              message: "Twitter is required mdfka" 
+            }
+          })} />
+          <p className='error'>{errors.social?.twitter?.message}</p>
         </div>
         <div className='form-control'>
           <label htmlFor="facebook">Facebook</label>
-          <input type="text" id="facebook" {...register("social.facebook")} />
+          <input type="text" id="facebook" {...register("social.facebook", {
+            required: {
+             value: true,
+              message: "Facebook is required mdfka"
+            }
+          })} />
+          <p className='error'>{errors.social?.facebook?.message}</p>
         </div>
+        <div className='form-control'>
+          <label htmlFor="primary-phone">Primary phone number</label>
+          <input type="text" id="primary-phone" {...register("phoneNumbers.0", {
+            required: "Primary phone number is required",
+          })} />
+          <p className='error'> {errors?.phoneNumbers?.["0"]?.message}</p>
+        </div>
+        <div className='form-control'>
+          <label htmlFor="secondary-phone">Secondary phone number</label>
+          <input type="text" id="secondary-phone" {...register("phoneNumbers.1", {
+            required: "Secondary phone number is required",
+          })} />
+          <p className='error'> {errors?.phoneNumbers?.["1"]?.message}</p>
+        </div>
+        <div>
+          <label htmlFor="">List of phone numbers</label>
+          <div>
+              {fields.map((field, index) => (
+                  <div className='form-control' key={field.id}>
+                    <input type="text" {...register(`phNumbers.${index}.number` as const )} />
+                    {index > 0 && <button type='button' onClick={()=> remove(index) }>Remove</button>}
+                  </div>
+                ))}
+                <button type='button' onClick={()=> append({number: ""})}>+Add phone number</button>
+          </div>
+        </div>
+        <div className='form-control'>
+          <label htmlFor="age">Age</label>
+          <input type="text" id="age" {...register("age", {
+            valueAsNumber: true,
+            required: "Age is required",
+          })} />
+          <p className='error'>{errors.age?.message}</p>
+        </div>
+        <div className='form-control'>
+          <label htmlFor="dob">DOB</label>
+          <input type="date" id="dob" {...register("dob", {
+            valueAsDate: true,
+            required: "Dob is required",
+          })} />
+          <p className='error'>{errors.dob?.message}</p>
+        </div>  
         <button>Submit</button>
+        <button type='button' onClick={handleGetValues}>Get Value</button>
       </form>
       <DevTool control={control}/>
     </div>
